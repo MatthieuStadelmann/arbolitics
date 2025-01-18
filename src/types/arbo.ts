9059afdfa195e486bebc6b492ef4908da2fbee6e
Matthieu Stadelmann <stadelmannmatthieu@gmail.com>
2025-01-18 18:02:23 +0100

    Remove comments from ArboDataPoint

--- src/types/arbo.ts
+++ src/types/arbo.ts
@@ -1,18 +1,18 @@
 export interface ArboDataPoint {
-    DID: string;     // Device ID (e.g. "25_225", "25_226")
-    FMW: number;     // Firmware version
-    TMS: number;     // Timestamp
-    bvol: number;    // Battery voltage
-    tem1: number;    // Temperature
-    hum1: number;    // Humidity
-    solr: number;    // Solar radiation
-    prec: number;    // Precipitation
-    wind: number;    // Wind speed
-    wins: number;    // Wind direction
-    lwet: number;    // Leaf wetness
-  }
+    DID: string;
+    FMW: number;
+    TMS: number;
+    bvol: number;
+    tem1: number;
+    hum1: number;
+    solr: number;
+    prec: number;
+    wind: number;
+    wins: number;
+    lwet: number;
+}
   
-  export interface ArboDataResponse {
+export interface ArboDataResponse {
     data: ArboDataPoint[];
     message: string;
-  }
+}
